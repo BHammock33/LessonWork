3d922bce3110578c6434f0e3dae199fd1635048e
BHammock33 <105878596+BHammock33@users.noreply.github.com>
2022-09-09 09:52:37 -0400

    Update WrapperExampleApplication.java

--- LessonWork/src/com/lesson7/WrapperExampleApplication.java
+++ LessonWork/src/com/lesson7/WrapperExampleApplication.java
@@ -24,6 +24,27 @@ public class WrapperExampleApplication{
 		
 		Integer anotherWrapperInt = Integer.valueOf(8); //not really necessary, the above works just as well
 		
+		Boolean someBoolean = null;
 		
+		if(Boolean.TRUE.equals(someBoolean)) {
+			System.out.println("True");
+		}else {
+			System.out.println("False");
+		}
+		
+		long firstLong = 10L;
+		long secondLong = 10L;
+		
+		if(firstLong == secondLong) {
+			System.out.println("Primitive works");
+		}
+		
+		Long firstLongWrapper = new Long(10);
+		Long secondLongWrapper = Long.valueOf(10);
+		
+		//will not work with == because the values are the same but the objects are different
+		if(firstLongWrapper.equals(secondLongWrapper)) {
+			System.out.println("Object comparison works");
+		}
 	}
 }
