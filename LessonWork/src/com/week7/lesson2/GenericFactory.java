77c23e1f259262ec680fcbfb5c6a1bfe5bee8c21
BHammock33 <105878596+BHammock33@users.noreply.github.com>
2022-09-13 10:30:37 -0400

    Create GenericFactory.java

---
+++ LessonWork/src/com/week7/lesson2/GenericFactory.java
@@ -0,0 +1,16 @@
+package com.week7.lesson2;
+
+public class GenericFactory {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+// interfaces are implemented not instantiated 
+// abstract classes must define at least one abstract method
+// non abstract methods have a defined body
+// interface defines a contract an abstract class is looser 
+// use abstract classes when you have a lot of similar but slightly different objects
+// interfaces are good for making people use the methods where the objects are very different 
